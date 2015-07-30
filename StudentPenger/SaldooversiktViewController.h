1bae8f42df3c9657080dace0c270c63a0cb924f0
Henriette Kopstad <henriettekopstad@Henriettes-MacBook-Pro.local>
2015-07-30 22:16:54 +0200

    Begynnelse storyboard

---
+++ StudentPenger/SaldooversiktViewController.h
@@ -0,0 +1,15 @@
+//
+//  SaldooversiktViewController.h
+//  StudentPenger
+//
+//  Created by Henriette Kopstad on 30/07/15.
+//  Copyright (c) 2015 Henriette Kopstad. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SaldooversiktViewController : UIViewController
+
+-(IBAction)instillinger:(id)sender;
+
+@end
---
+++ StudentPenger/SaldooversiktViewController.m
@@ -0,0 +1,42 @@
+//
+//  SaldooversiktViewController.m
+//  StudentPenger
+//
+//  Created by Henriette Kopstad on 30/07/15.
+//  Copyright (c) 2015 Henriette Kopstad. All rights reserved.
+//
+
+#import "SaldooversiktViewController.h"
+
+@interface SaldooversiktViewController ()
+
+@end
+
+@implementation SaldooversiktViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+//TODO = Få denne til å gå til neste view i stedet
+//-(IBAction)instillinger:(id)sender{
+//    [self.navigationController dismissViewControllerAnimated:YES completion: nil];
+//}
+
+@end
