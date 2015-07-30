311205b8d49a4ba54df71ecca01ff0fd8c345ce8
Henriette Kopstad <henriettekopstad@Henriettes-MacBook-Pro.local>
2015-07-30 19:23:14 +0200

    Initial Commit

---
+++ StudentPenger/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  StudentPenger
+//
+//  Created by Henriette Kopstad on 30/07/15.
+//  Copyright (c) 2015 Henriette Kopstad. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
